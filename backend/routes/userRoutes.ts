28d0b1e11b01ed049a71323046f92e10cdea03ec
FarmanOthman <farmaanothmaan@gmail.com>
2025-03-20 15:04:15 +0300

    refactor user routes: add authentication and authorization middleware for admin access and update login endpoint

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -6,14 +6,21 @@ import {
   updateUser,
   deleteUser,
 } from '../controllers/userController';
+import authMiddleware from '../middlewares/authMiddleware';
+import authorizationMiddleware from '../middlewares/authorizationMiddleware';
+import { UserRole } from '../types';
 
 const router = express.Router();
 
 // CRUD Routes
 router.get('/users', getUsers as RequestHandler);
 router.post('/users', createUser as RequestHandler);
-router.post('/users/login', loginUser as RequestHandler);
+router.post('/login', loginUser as RequestHandler);
 router.put('/users/:id', updateUser as RequestHandler);
 router.delete('/users/:id', deleteUser as RequestHandler);
+
+router.get('/admin', authMiddleware, authorizationMiddleware([UserRole.ADMIN]), (req, res) => {
+    res.send('Welcome Admin');
+});
 
 export default router;
